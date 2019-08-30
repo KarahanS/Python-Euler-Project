4e5af56ea3bfd4f28d9347950ca83ebb31ba084f
Karahan Sarıtaş <44376034+KarahanS@users.noreply.github.com>
2019-08-30 11:22:03 +0300

    Add files via upload

---
+++ Euler Project Problem 7.py
@@ -0,0 +1,24 @@
+
+# Project Euler Problem 7
+
+import math
+import time
+start_time = time.time()
+list=[2]
+a=1
+while len(list)<10001:
+    a=a+2
+    t = 2
+    while t<=round(math.sqrt(a)):
+        k=True
+        if a%t==0 and t!=a:
+            k=False
+            break
+        t=t+1
+    if k==True:
+        list.append(a)
+
+
+print(list[-1])
+
+print("--- %f seconds ---" % (time.time() - start_time))
