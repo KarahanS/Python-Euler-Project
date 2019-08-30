5652cc593836dbbeed420f9ef9c23f9098e55633
Karahan Sarıtaş <44376034+KarahanS@users.noreply.github.com>
2019-08-30 11:13:25 +0300

    Add files via upload

---
+++ Euler Project Problem 4.py
@@ -0,0 +1,20 @@
+
+# Project Euler Problem 3
+
+import time
+start_time = time.time()
+i=range(100,1000)
+l=[]
+for k in i:
+    for t in i:
+        multiple=k*t
+        end=str(multiple)
+        reverse=end[::-1]
+        if end==reverse:
+            l.append(int(end))
+        else:
+            continue
+print(sorted(l)[-1])
+
+print("--- %f seconds ---" % (time.time() - start_time))
+
