e39295245c10418b68c12663071493370b4acb6e
Karahan Sarıtaş <44376034+KarahanS@users.noreply.github.com>
2019-08-30 11:19:52 +0300

    Add files via upload

---
+++ Euler Project Problem 6.py
@@ -0,0 +1,17 @@
+
+# Project Euler Problem 
+
+import time
+start_time = time.time()
+t=range(1,101)
+sum_of_the_squares=0
+for i in t:
+    sum_of_the_squares+=i**2
+sum=0
+for i in t:
+    sum+=i
+square_of_the_sum=sum**2
+print(square_of_the_sum-sum_of_the_squares)
+
+print("--- %f seconds ---" % (time.time() - start_time))
+
