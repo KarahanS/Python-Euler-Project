5627671bffbd2c630723d8fdf1c04232cfb4eb5b
Karahan Sarıtaş <44376034+KarahanS@users.noreply.github.com>
2019-08-30 11:17:41 +0300

    Add files via upload

---
+++ Euler Project Problem 5.py
@@ -0,0 +1,38 @@
+
+# Project Euler Problem 3
+
+import time
+start_time = time.time()
+l={}
+for number in range(1,21):
+    n = number
+    t = 2
+    k=1
+    m=0
+    while n > 1:
+        if n % t == 0:
+            n = n / t
+            if t in l and k==1:
+                m=m+1
+                if m>l[t]:
+                    l[t]=m
+            else:
+                l[t]=k
+                k=k+1
+                m=0
+
+
+        else:
+            t = t + 1
+            k=1
+            m=0
+a=1
+for k in l:
+    m=k**l[k]
+    a=m*a
+
+print(a)
+
+
+print("--- %f seconds ---" % (time.time() - start_time))
+
