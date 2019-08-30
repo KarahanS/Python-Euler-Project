36f9830f534c4f6f5e6dfb19c3625a90fbf2d15c
Karahan Sarıtaş <44376034+KarahanS@users.noreply.github.com>
2019-08-30 11:26:36 +0300

    Add files via upload

---
+++ Euler Project Problem 8.py
@@ -0,0 +1,38 @@
+
+# Project Euler Problem 7
+
+import time
+start_time = time.time()
+
+f=open("C:\Program Files\Python 3.7\Project Euler Problem 8 Number.txt")
+a=f.readlines()
+
+
+k=0
+while k<len(a):
+    a[k]=a[k].strip()
+    k=k+1
+
+a="".join(a)
+number_list=list(a)
+
+t={}
+multiplelist=[]
+i=0
+while i<988:
+    new_list=number_list[i:i+13]
+    sayı="".join(new_list)
+    i=i+1
+    multiple=1
+    for k in new_list:
+        multiple=multiple*int(k)
+    t[sayı]=multiple
+
+a=sorted(t.values())
+for l in t:
+    if a[-1]==t[l]:
+        print(l," --> multiplication of these numbers =",t[l])
+
+
+print("--- %f seconds ---" % (time.time() - start_time))
+
